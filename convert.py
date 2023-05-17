e62d59753192c5a306ad3b22c84008c9ea270736
Joey Aramouni <joeyaram@bu.edu>
2023-05-17 13:09:49 -0500

    Reformat

--- convert.py
+++ convert.py
@@ -1,12 +1,12 @@
-import argparse
-import datetime
 import pandas as pd
 from collections import defaultdict
 import re
 import os
-from pathlib import Path
 
-GENERAL_WA_MULTI_SEARCH_PATTERN = r'\[(\d{1,2}\/\d{1,2}\/\d{2}), (\d{1,2}:\d{2}:\d{2} [APM]{2})\] (.*?): (.*)'
+
+GENERAL_WA_MULTI_SEARCH_PATTERN = (
+    r"\[(\d{1,2}\/\d{1,2}\/\d{2}), (\d{1,2}:\d{2}:\d{2} [APM]{2})\] (.*?): (.*)"
+)
 LINE_SPLIT_DELIMITER = "\n"
 IGNORE_LIST = ["omitted", "Missed voice call"]
 RESPONDER = "Joey Aramouni"
@@ -24,44 +24,45 @@ def text_to_dictionary(text, response):
     if isinstance(text, (bytes, bytearray)):
         text = text.decode()
 
-    text_list = text.split('\n')[1:]
-    result_dict, count, prev_author = defaultdict(dict), 0, ''
-    for ix, line in enumerate(text_list):
+    text_list = text.split("\n")[1:]
+    result_dict, count, prev_author = defaultdict(dict), 0, ""
+    for line in text_list:
         search_pattern = re.search(GENERAL_WA_MULTI_SEARCH_PATTERN, line)
         if search_pattern is not None:
             author = search_pattern.group(3)
-            message = search_pattern.group(4).replace('"','')
+            message = search_pattern.group(4).replace('"', "")
             if any(keyword in message for keyword in IGNORE_LIST):
                 continue
         else:
             continue
         if author != response:
             if author == prev_author:
-                prev = result_dict[count]['prompt'][:-7]
-                result_dict[count]['prompt'] = f"{prev}. {message}\n\n###\n\n"
+                prev = result_dict[count]["prompt"][:-7]
+                result_dict[count]["prompt"] = f"{prev}. {message}\n\n###\n\n"
             else:
                 count += 1
-                result_dict[count].update({'prompt': message + "\n\n###\n\n"})
+                result_dict[count].update({"prompt": message + "\n\n###\n\n"})
 
         else:
             if author == prev_author:
-                prev = result_dict[count]['completion'][:-4]
-                result_dict[count]['completion'] = f"{prev}. {message} ###"
+                prev = result_dict[count]["completion"][:-4]
+                result_dict[count]["completion"] = f"{prev}. {message} ###"
             else:
-                result_dict[count].update({'completion': message + " ###"})
+                result_dict[count].update({"completion": message + " ###"})
 
         prev_author = author
     return result_dict
 
+
 def parse_whatsapp_text_into_dataframe(raw_text, responder):
     result_dict = text_to_dictionary(raw_text, responder)
-    df = pd.DataFrame.from_dict(result_dict).T[['prompt', 'completion']]
+    df = pd.DataFrame.from_dict(result_dict).T[["prompt", "completion"]]
     return df
 
 
 def converter(
-        filepath: str,
-        responder: str,
+    filepath: str,
+    responder: str,
 ) -> pd.DataFrame:
     """
     Turn whatsapp chat data into a format
@@ -71,12 +72,13 @@ def converter(
     :return: a parsed pandas dataframe
     """
 
-    with open(filepath, 'r') as fp:
+    with open(filepath, "r") as fp:
         text = fp.read()
 
     df = parse_whatsapp_text_into_dataframe(text, responder)
     df = df.dropna()
     return df
+
 
 if __name__ == "__main__":
     df_final = pd.DataFrame()
@@ -84,4 +86,6 @@ if __name__ == "__main__":
         print(f"Preprocessing conv with {file.split('.')[0]}")
         df = converter(f"./chats/{file}", RESPONDER)
         df_final = df if not len(df_final) else pd.concat([df_final, df])
-    df_final.to_json(f'output/output.json',lines=True,orient='records', force_ascii=False)
+    df_final.to_json(
+        f"output/output.json", lines=True, orient="records", force_ascii=False
+    )
